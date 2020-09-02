3bd98fdae62401e2d9864ef6f2d20f904c0cbdc8
KagChi <59391215+KagChi@users.noreply.github.com>
2020-09-02 19:24:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const request = require("node-superfetch");
 const { author, version } = require("./package.json");
 async function botData(idBot) {
 if (!idBot) throw Error('Enter Bot Id');
-   const { body: data } = await request.get('https://xysebotlist.ga/api/by-id?q=' + idBot)
-   return data;
+const {body: data} = await request.get("https://xysebotlist.ga/api/by-id?q=" + idBot)
+return data;
+   //const { body: data } = await request.get('https://xysebotlist.ga/api/by-id?q=' + idBot)
+   //return data;
 }
 
 module.exports.botData = botData;
