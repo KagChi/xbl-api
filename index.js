dc9a505d138348694e042bbf7ce7196a638f0bbd
KagChi <59391215+KagChi@users.noreply.github.com>
2020-09-02 18:19:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
 async function botData(idBot) {
 if (!idBot) throw Error('Enter Bot Id');
-   
-await axios.get('https://xysebotlist.ga/api/by-id?q=' + idBot)
+   const data = await axios.get('https://xysebotlist.ga/api/by-id?q=' + idBot).then(res => res)
+   return data;
 }
 
 module.exports.botData = botData;
-module.exports.version = "0.0.1";
+module.exports.version = "0.0.2";
